65eaaabc3e3158af55b624163cfa1f9f27a9a39d
Mina Magdy <minamagdypotros92@gmail.com>
2025-08-17 16:25:21 +0300

    otp2 UI

---
+++ lib/features/auth/presentation/screens/change_pass.dart
@@ -0,0 +1,81 @@
+import 'package:flexana/core/theme/Colors.dart';
+import 'package:flutter/material.dart';
+
+class ChangePass extends StatefulWidget {
+  const ChangePass({super.key});
+
+  @override
+  State<ChangePass> createState() => _ChangePassState();
+}
+
+class _ChangePassState extends State<ChangePass> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.Primarycolor,
+      resizeToAvoidBottomInset: true,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.035),
+        child: ListView(
+          children: [
+            SizedBox(height: screenHeight * 0.15),
+            AssetsData.logo(context),
+            SizedBox(height: screenHeight * 0.14),
+            Column(
+              children: [
+                CustomTextFormField(
+                  hintText: 'Phone Number',
+                  icon: 'assets/Icons/profile_icon.png',
+                ),
+                SizedBox(height: screenHeight * 0.025),
+
+                CustomTextFormField(
+                  hintText: 'Password',
+                  icon: 'assets/Icons/password_icon.png',
+                ),
+              ],
+            ),
+
+            SizedBox(height: screenHeight * 0.07),
+
+            CustomImageButton(title: 'Login'),
+            SizedBox(height: screenHeight * 0.008),
+
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  SlideRightRoute(page: ForgetPassScreen()),
+                );
+              },
+              child: Text("forgot password", style: normalTextStyle()),
+            ),
+
+            SizedBox(height: screenHeight * 0.06),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    CustomSwitch(),
+                    Text("Remember me", style: normalTextStyle()),
+                  ],
+                ),
+                SizedBox(width: screenWidth * 0.04), // ~14.w
+                TextButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      SlideRightRoute(page: SignupScreen()),
+                    );
+                  },
+                  child: Text("Create Account", style: normalTextStyle()),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/auth/presentation/screens/otp2_screen.dart
+++ lib/features/auth/presentation/screens/otp2_screen.dart
@@ -3,24 +3,22 @@ import 'package:flexana/core/theme/Textstyle.dart';
 import 'package:flexana/core/utils/assets_data.dart';
 import 'package:flexana/core/widgets/custom_button.dart';
 import 'package:flexana/core/widgets/custom_slide_inemation.dart';
-import 'package:flexana/features/auth/presentation/screens/Otp1_screen.dart';
-import 'package:flexana/features/auth/presentation/screens/otp2_screen.dart';
+import 'package:flexana/features/auth/presentation/screens/login_screen.dart';
 import 'package:flexana/features/auth/presentation/widgets/Custom_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class ForgetPassScreen extends StatefulWidget {
-  const ForgetPassScreen({super.key});
-  static String id = "otp_login_screen";
+class Otp2Screen extends StatefulWidget {
+  const Otp2Screen({super.key});
+
   @override
-  State<ForgetPassScreen> createState() => _ForgetPassScreen();
+  State<Otp2Screen> createState() => _Otp2ScreenState();
 }
 
-class _ForgetPassScreen extends State<ForgetPassScreen> {
+class _Otp2ScreenState extends State<Otp2Screen> {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       backgroundColor: AppColors.Primarycolor,
       resizeToAvoidBottomInset: true,
@@ -28,26 +26,38 @@ class _ForgetPassScreen extends State<ForgetPassScreen> {
         padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.035),
         child: ListView(
           children: [
-            SizedBox(height: 175..h),
+            SizedBox(height: 175.h),
             AssetsData.logo(context),
             SizedBox(height: 95.h),
             Column(
               children: [
                 CustomTextFormField(
-                  hintText: 'Phone Number',
-                  icon: 'assets/Icons/Phonee_icon.png',
+                  hintText: 'Enter OTP Code',
+                  icon: 'assets/Icons/Otp_icon.png',
                 ),
               ],
             ),
-
-            SizedBox(height: 75.h),
+            SizedBox(height: 70.h),
             CustomImageButton(
               title: 'Confirm',
               onTap: () {
-                Navigator.push(context, SlideRightRoute(page: Otp2Screen()));
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  SlideRightRoute(page: LoginScreen()),
+                  (route) => false,
+                );
               },
             ),
-
+            CustomImageButton(
+              title: 'Send again',
+              onTap: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  SlideRightRoute(page: LoginScreen()),
+                  (route) => false,
+                );
+              },
+            ),
             Center(child: Text('Or', style: normalTextStyle())),
 
             CustomImageButton(
